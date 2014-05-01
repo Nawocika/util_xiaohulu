d028761fc09295b543b7eae97fa1c809d06b7f11
xiaohulu <liweityut@163.com>
2014-05-01 21:53:39 +0800

    动态代理模式

---
+++ src/com/designmodel/proxy/Test_Cglib.java
@@ -0,0 +1,13 @@
+package com.designmodel.proxy;
+
+/**
+ * Created by lw on 14-5-1.
+ */
+public class Test_Cglib {
+
+    public static void main(String[] args) {
+        BookFacadeProxy_Cglib proxy_cglib = new BookFacadeProxy_Cglib();
+        BookImpl bookImpl = (BookImpl) proxy_cglib.getInstance(new BookImpl());
+        bookImpl.seeBook();
+    }
+}
